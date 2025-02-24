2d2e9695d054279e9a287214e1acf36e5be408f7
Kuro-no.Kenshi <dani04ranzo@gmail.com>
2025-02-24 15:01:28 +0100

    aggiunte cors

--- GestioneAzienda/Program.cs
+++ GestioneAzienda/Program.cs
@@ -11,12 +11,24 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
