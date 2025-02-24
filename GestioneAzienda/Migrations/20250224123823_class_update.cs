270b159027d2cd4819c2849c76bcbbae5e0e13ee
Kuro-no.Kenshi <dani04ranzo@gmail.com>
2025-02-24 14:00:10 +0100

    create migration

---
+++ GestioneAzienda/Migrations/20250224123823_class_update.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestioneAzienda.Migrations
+{
+    /// <inheritdoc />
+    public partial class class_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleMaintenances_Vehicles_VehicleId",
+                table: "VehicleMaintenances");
+
+            migrationBuilder.DropColumn(
+                name: "VehiclesId",
+                table: "VehicleMaintenances");
+
+            migrationBuilder.DropColumn(
+                name: "IsPermanentContract",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "VehicleMaintenances",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "CourseStart",
+                table: "Employees",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "Employees",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "ContractEnd",
+                table: "Employees",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContractId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Contracts",
+                columns: table => new
+                {
+                    ContractId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contracts", x => x.ContractId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicalExaminations",
+                columns: table => new
+                {
+                    MedicalExaminationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MedicalDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalExaminations", x => x.MedicalExaminationId);
+                    table.ForeignKey(
+                        name: "FK_MedicalExaminations_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalExaminations_EmployeeId",
+                table: "MedicalExaminations",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleMaintenances_Vehicles_VehicleId",
+                table: "VehicleMaintenances",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "VehicleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleMaintenances_Vehicles_VehicleId",
+                table: "VehicleMaintenances");
+
+            migrationBuilder.DropTable(
+                name: "Contracts");
+
+            migrationBuilder.DropTable(
+                name: "MedicalExaminations");
+
+            migrationBuilder.DropColumn(
+                name: "ContractEnd",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ContractId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "VehicleMaintenances",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VehiclesId",
+                table: "VehicleMaintenances",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "CourseStart",
+                table: "Employees",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPermanentContract",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleMaintenances_Vehicles_VehicleId",
+                table: "VehicleMaintenances",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "VehicleId");
+        }
+    }
+}
